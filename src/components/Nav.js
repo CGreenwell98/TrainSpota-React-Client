d53e9a2258d6d90cefb50be4429bc8638f818687
CGreenwell98 <christophergreenwell7@gmail.com>
2020-12-06 22:38:14 +0000

    initial client commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import "../styles/nav.css";
+
+function Nav() {
+    return (
+      <nav>
+        <h1>Project</h1>
+        <ul>
+            <li>Home</li>
+            <li>Sign-In</li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Nav;
